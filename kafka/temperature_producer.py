399bdc2e611a724069bd5babb86131ed3df6b50c
Clara V <clara.cooleyah@gmail.com>
2024-11-04 09:50:33 +0700

    Update temperature_producer.py

--- kafka/temperature_producer.py
+++ kafka/temperature_producer.py
@@ -7,10 +7,9 @@ import random
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-sensor_ids = ['S1', 'S2', 'S3']  # ID sensor
+sensor_ids = ['S1', 'S2', 'S3'] 
 
 while True:
-    # Simulasi suhu acak antara 60°C dan 100°C
     for sensor_id in sensor_ids:
         temperature = random.randint(60, 100)
         data = {
@@ -22,4 +21,4 @@ while True:
         producer.send('sensor-suhu', value=data)
         print(f'Sent: {data}')
         
-    time.sleep(1)  # Kirim setiap detik
+    time.sleep(1) 
